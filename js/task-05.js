17260a62f4f83c4cee386d45010b7b51d89c9a12
oksyluka <oksana_l2@ukr.net>
2022-07-08 12:47:12 +0300

    task 5 is fixed

--- js/task-05.js
+++ js/task-05.js
@@ -1,3 +1,9 @@
 const textInput = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
-textInput.addEventListener('input', (event) => {output.textContent = event.currentTarget.value });
+
+textInput.addEventListener('input', (event) => {
+    if (event.currentTarget.value === '') {
+        output.textContent = 'Anonymous'
+    }
+    output.textContent = event.currentTarget.value;
+});
---
+++ js/task-06.js
@@ -0,0 +1,11 @@
+const textInput = document.querySelector('#validation-input');
+const checkInput = textInput.value.length;
+
+textInput.addEventListener('input', (event) => {
+    const cur = event.currentTarget.value;
+}
+    
+
+textInput.addEventListener('blur', () => {
+    
+})
