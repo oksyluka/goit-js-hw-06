95dbb8cb16105604e7bf549eaad0167838e260b8
oksyluka <oksana_l2@ukr.net>
2022-07-11 16:20:34 +0300

    task 10 is fixed

--- js/task-10.js
+++ js/task-10.js
@@ -7,10 +7,9 @@ const createBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
 const resultBoxes = document.querySelector('#boxes');
 
-function createBoxes() {
-  const elementInList = resultBoxes.childElementCount;
-  const value = inputValue.value;
-  for (let i = elementInList; i < +value + elementInList; i += 1) {
+function createBoxes(amount) {
+  amount = inputValue.value;
+  for (let i = 1; i <= amount; i += 1) {
     resultBoxes.insertAdjacentHTML(
       'beforeend',
       `<div style="width: ${i * 10 + 30 + 'px'};
